2df826b79954e3965d21b1a1ba6a45d6a02db12e
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2025-02-03 07:22:43 +0000

    ui done

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,15 +2,17 @@
 import {HeroUIProvider} from "@heroui/react";
 
 import {Button, ButtonGroup} from "@heroui/button";
-
+import Emoji from "../components/Emoji";
 
 export default function Home() {
+
+
   return (
     <HeroUIProvider>
     <div className="flex flex-col items-center justify-center gap-4 p-4 min-h-screen">
-      <h1 className="text-4xl font-bold">Made Room</h1>
-      <Button color="primary">Button</Button>
-      
+
+      <Emoji />
+
     </div>
   </HeroUIProvider>
   );
---
+++ client/components/Emoji.jsx
@@ -0,0 +1,38 @@
+import { Button, Card } from "@heroui/react";
+import { useState } from "react";
+
+function Emoji() {
+  const [emoji, setEmoji] = useState("🐸");
+
+  return (
+    <div className="min-h-screen flex items-center justify-center flex-col gap-5">
+      <h1 className="text-gray-600 uppercase text-sm">MADE App</h1>
+      <EmojiPreview emoji={emoji} />
+      <EmojiSelect onClick={setEmoji} />
+    </div>
+  );
+}
+
+function EmojiPreview({ emoji }) {
+  return <Card className="text-7xl p-4">{emoji}</Card>;
+}
+
+function EmojiSelect({ onClick }) {
+  const emojiOptions = "🌝,🐙,🐣,🐱,🙉,🐠".split(",");
+
+  return (
+    <div className="flex gap-1 flex-wrap justify-center">
+      {emojiOptions.map((emoji) => (
+        <Button
+          className="text-2xl"
+          variant="faded"
+          onPress={() => onClick(emoji)}
+        >
+          {emoji}
+        </Button>
+      ))}
+    </div>
+  );
+}
+
+export default Emoji;

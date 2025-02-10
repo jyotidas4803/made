a8a8fcd757a5a1f4facb36f4fd2668acb0b26b39
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2025-02-10 07:54:50 +0000

    add session storage to persist the user

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,3 +1,4 @@
+
 import {
   Card,
   CardHeader,
@@ -10,8 +11,17 @@ import {
   Input,
   Button,
 } from "@heroui/react";
+import { useEffect } from "react";
 
 function SignUp({ setUser, socket }) {
+  // Check from session storage
+  useEffect(() => {
+    const session = sessionStorage.getItem("user");
+    if (session) {
+      setUser(session);
+    }
+  }, []);
+
   const onSubmit = (e) => {
     // Prevent default browser page refresh.
     e.preventDefault();
@@ -19,9 +29,14 @@ function SignUp({ setUser, socket }) {
     // Get form data as an object.
     const data = Object.fromEntries(new FormData(e.currentTarget));
 
-    // Emit this to our server.
-    setUser(data);
-    socket.emit("new_user", data);
+    // Set into session
+    sessionStorage.setItem("user", data.name);
+
+    // Emit to the server
+    socket.emit("user", data.name);
+
+    // Update parent's state
+    setUser(data.name);
   };
 
   return (
@@ -36,8 +51,8 @@ function SignUp({ setUser, socket }) {
             width={40}
           />
           <div className="flex flex-col">
-            <p className="text-md">MADE-Room</p>
-            <p className="text-small text-default-500">made-two.vercel.app</p>
+            <p className="text-md">MADE Room</p>
+            <p className="text-small text-default-500">made.phleebs.tech</p>
           </div>
         </CardHeader>
 
@@ -65,7 +80,7 @@ function SignUp({ setUser, socket }) {
           <Link
             isExternal
             showAnchorIcon
-            href="https://github.com/jyotidas4803/made"
+            href="https://github.com/gliese-sketch/made"
           >
             Visit source code on GitHub.
           </Link>

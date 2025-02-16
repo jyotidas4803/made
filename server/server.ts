85b86b728a8144a3e9dd2b1fa3078a73ae2d4bfe
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2025-02-16 06:28:42 +0000

    thank u

--- server/server.ts
+++ server/server.ts
@@ -3,12 +3,12 @@ import express from "express";
 import axios from "axios";
 import { Server } from "socket.io";
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 const server = http.createServer(app);
 
-const URL = "https://api.wakati.tech/ai";
+const URL = `https://api.cloudflare.com/client/v4/accounts/${process.env.ACCOUNT_ID}/ai/run/@cf/meta/llama-3.1-8b-instruct-fast`;
 
 app.get("/", (req, res) => {
   res.send("Socket.io server is healthy!");
@@ -23,35 +23,43 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`New client connected: ${socket.id}`);
 
-  socket.on("user", (data) => {
-    socket.broadcast.emit("new_user", data);
+  socket.on("user", (name) => {
+    socket.broadcast.emit("new_user", name);
   });
 
   socket.on("message", async (data) => {
     if (data.type === "text" && data.content.startsWith("@ai")) {
+      socket.broadcast.emit("new_message", data); // Send the ai prompt message
+
       const query = {
-        prompt: data.content.replaceAll("@ai"),
+        prompt: data.content.replaceAll("@ai", ""),
       };
 
       const options = {
         headers: {
           "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.TOKEN}`,
         },
       };
 
       const response = await axios.post(URL, query, options);
-      const newMessage = { ...data, content: response };
 
-      console.log(response.data.res.response);
+      console.log(response);
 
-      socket.broadcast.emit("new_message", newMessage);
-    }
-    {
+      const newMessage = {
+        ...data,
+        type: "ai",
+        content: response.data.result.response,
+      };
+
+      io.emit("new_message", newMessage); // Send the ai response back
+    } else {
       socket.broadcast.emit("new_message", data);
     }
   });
 
   socket.on("typing", (data) => {
+    console.log(data);
     socket.broadcast.emit("user_typing", data);
   });
 });
